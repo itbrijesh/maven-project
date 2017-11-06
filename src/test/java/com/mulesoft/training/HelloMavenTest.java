1e527ddf70f7e10dc27c5d1187c5b0cbc077ff41
Prajapati <bprajapati@deloitte.com>
2017-11-06 16:57:57 -0500

    Add test case for Outbound property

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
 
@@ -17,6 +18,16 @@ public class HelloMavenTest extends FunctionalTestCase {
     	
     	System.out.println("\n \n Dynamic Port is >>>>> " + myPort.getNumber() );
         runFlowAndExpect("mavenFlow", "Hello Maven");
+    }
+    
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+    	
+    	System.out.println("\n \n *** TEST CASE 2 ***** Dynamic Port is >>>>> " + myPort.getNumber() );
+    	
+      MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
     }
     
     @Override
